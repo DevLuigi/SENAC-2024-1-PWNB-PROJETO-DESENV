65a1d3eb4790bc46c75fe6466c20150ee4689b4b
DevLuigi <luigidasilvacoelho@gmail.com>
2024-05-12 21:14:34 -0300

    feat: Adiciona gráficos a tela de AED

--- pages/analiseExploratoriaDados/index.js
+++ pages/analiseExploratoriaDados/index.js
@@ -1,1 +1,12 @@
-console.log("lógica da tela");
+const simularDados = () => {
+    const dados = [
+        ['Legenda', 'Qtd. Torcedores'],
+        ['Corinthians', 100],
+        ['Palmeiras', 30],
+        ['São paulo', 25],
+        ['Santos', 15]
+    ];
+
+    // hard-coded por enquanto, proximo passo é criar o BD temporario
+    return dados;
+}
