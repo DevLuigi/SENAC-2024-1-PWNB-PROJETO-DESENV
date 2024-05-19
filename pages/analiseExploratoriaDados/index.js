5df74cafa1bd5902506d19a5c6f25b9483008c71
DevLuigi <luigidasilvacoelho@gmail.com>
2024-05-19 19:51:20 -0300

    feat: Adiciona localStorage a tela de AED

--- pages/analiseExploratoriaDados/index.js
+++ pages/analiseExploratoriaDados/index.js
@@ -1,12 +1,42 @@
-const simularDados = () => {
-    const dados = [
-        ['Legenda', 'Qtd. Torcedores'],
-        ['Corinthians', 100],
-        ['Palmeiras', 30],
-        ['São paulo', 25],
-        ['Santos', 15]
-    ];
+const agruparDados = () => {
+    const vendas = JSON.parse(localStorage.getItem("vendas"));
 
-    // hard-coded por enquanto, proximo passo é criar o BD temporario
+    const agrupamento = {};
+    vendas.forEach((venda) => {
+        const {produto, qtdVenda, valorTotal} = venda;
+
+        if (!agrupamento[produto]) {
+            agrupamento[produto] = {
+                quantidadeVendida: 0,
+                valorTotalVendido: 0
+            }
+        }
+
+        agrupamento[produto].quantidadeVendida += parseInt(qtdVenda);
+        agrupamento[produto].valorTotalVendido += parseInt(valorTotal);
+    })
+
+    return agrupamento;
+}
+
+const carregarDados = (legenda) => {
+    const vendasAgrupadas = agruparDados();
+    
+    let dados = [];
+    if (legenda == "valor") {
+        // retorna um array dos próprios pares [key, value] e cria um novo array com eles
+        dados = Object.entries(vendasAgrupadas).map(([produto, valores]) => {
+            return [produto, valores.valorTotalVendido];
+        });   
+    } else {
+        // retorna um array dos próprios pares [key, value] e cria um novo array com eles
+        dados = Object.entries(vendasAgrupadas).map(([produto, valores]) => {
+            return [produto, valores.quantidadeVendida];
+        });   
+    }
+
+    // adiciona legenda ao array
+    dados.unshift(['Legenda', legenda]);
+
     return dados;
 }
